90b8003dfd72ba9d40b3076ebc0024c35e51f3ba
AndiZogaj22 <az51520@ubt-uni.net>
2023-04-16 23:51:23 +0200

    website preloader done

---
+++ js/loader.js
@@ -0,0 +1,11 @@
+
+        // hide preloader when page is loaded
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+    document.querySelector("#loader-wrapper").style.display = "block";
+  } else {
+    document.querySelector("#loader-wrapper").style.display = "none";
+  }
+};
+
+    
