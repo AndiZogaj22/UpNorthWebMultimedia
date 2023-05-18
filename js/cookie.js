58f8d30156b7a1eadf6d9974e6cdc9d9d78b2af5
DimalMaloku1 <dm51522@ubt-uni.net>
2023-05-18 16:55:47 +0200

    Cookie js

---
+++ js/cookie.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+document.querySelector(".popup").style.
+display = "block"
+        },
+        7000
+    )
+})
+        document.querySelector("#close-cookie").addEventListener
+        ("click", function(){
+            document.querySelector(".popup").style.display=
+            "none"
+        })
+        document.querySelector(".accept").addEventListener
+        ("click", function(){
+            document.querySelector(".popup").style.display=
+            "none"
+        })
